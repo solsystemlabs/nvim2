2eccf43a01f9758980517a403d8e3cff8ce809c0
Taylor Eernisse <teernisse@visiostack.com>
2025-03-18 16:11:28 -0400

    tweaks

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -19,12 +19,12 @@ return {
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
       },
       'saadparwaiz1/cmp_luasnip',
@@ -35,6 +35,8 @@ return {
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-nvim-lsp-signature-help',
+      'mtoohey31/cmp-fish',
+      'andersevenrud/cmp-tmux',
     },
     config = function()
       -- See `:help cmp`
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -219,7 +219,10 @@ return {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
+        html = { filetypes = { 'html' } },
+        tailwindcss = { filetypes = { 'html', 'vue', 'javascript', 'typescript', 'typescriptreact', 'javascriptreact' } },
+
         --
 
         lua_ls = {
