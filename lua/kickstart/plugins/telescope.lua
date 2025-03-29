f146fd4cc70692fdbbb25dd39e61231a63cf3913
Taylor Eernisse <teernisse@visiostack.com>
2025-03-29 15:24:04 -0400

    Modified text of keybinds for whichkey

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -238,19 +238,19 @@ return {
       end
 
       -- Set up keymaps
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader>sm', builtin.git_status, { desc = '[S]earch [M]odified files' })
+      -- vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = 'Keymaps' })
+      -- vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      -- vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = 'Current Word' })
+      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Grep' })
+      -- vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = 'Resume' })
+      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = 'Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>sm', builtin.git_status, { desc = 'Modified files' })
 
       -- Add the unsaved buffers command, bound to <leader>su
-      vim.keymap.set('n', '<leader>su', find_unsaved_buffers, { desc = '[S]earch [U]nsaved buffers with diff' })
+      vim.keymap.set('n', '<leader>su', find_unsaved_buffers, { desc = 'Unsaved buffers with diff' })
 
       -- Enhanced buffers mapping with delete capability
       vim.keymap.set('n', '<leader><leader>', function()
@@ -285,7 +285,7 @@ return {
       -- Search neovim config files
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[S]earch [N]eovim files' })
+      end, { desc = 'Neovim files' })
 
       -- Search in open buffers
       vim.keymap.set('n', '<leader>sb', function()
