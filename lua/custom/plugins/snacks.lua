da4b129cc55a894b33395a688f53d870a286e3ec
Taylor Eernisse <teernisse@visiostack.com>
2025-03-31 10:55:13 -0400

    snacks stuff

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -36,7 +36,21 @@ return { {
         return vim.g.snacks_scroll ~= false and vim.b[buf].snacks_scroll ~= false and vim.bo[buf].buftype ~= "terminal"
       end
     },
-    statuscolumn = { enabled = true },
+    statuscolumn = {
+      left = { "mark", "sign" }, -- priority of signs on the left (high to low)
+      right = { "fold", "git" }, -- priority of signs on the right (high to low)
+      folds = {
+        open = true,             -- show open fold icons
+        git_hl = true,           -- use Git Signs hl for fold icons
+      },
+      git = {
+        -- patterns to match Git signs
+        patterns = { "GitSign", "MiniDiffSign" },
+      },
+      refresh = 50, -- refresh at most every 50ms
+    },
+    terminal = { enabled = true },
+    win = { enabled = true },
     words = { enabled = true },
     styles = {
       notification = {
