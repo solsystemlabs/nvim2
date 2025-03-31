1ccec89a6c6bd7290d6c0882eed4b64f5125a48a
Elias Witt <ewitt@visiostack.com>
2025-03-31 09:02:41 -0400

    remove mini, add snacks.nvim

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -39,8 +39,6 @@ require('lazy').setup({
 
   require 'kickstart/plugins/todo-comments',
 
-  require 'kickstart/plugins/mini',
-
   require 'kickstart/plugins/treesitter',
 
   require 'kickstart/plugins/ts-comments',
