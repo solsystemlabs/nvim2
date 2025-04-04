189bb3263557d0cdf26cb95fbe64e3d07b27d8e9
Taylor Eernisse <teernisse@visiostack.com>
2025-04-04 17:42:27 -0400

    Add tab management keybinding

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -71,4 +71,39 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- Rename element keybinding
 -- vim.keymap.set('n', '<leader>rr', vim.lsp.buf.rename, { desc = 'Rename Element' })
 
+-- =====================================================
+-- Tab management keybindings
+-- =====================================================
+
+-- Create a new tab
+vim.keymap.set('n', '<leader>tn', ':tabnew<CR>', { desc = '[T]ab [N]ew', silent = true })
+
+-- Close current tab
+vim.keymap.set('n', '<leader>tc', ':tabclose<CR>', { desc = '[T]ab [C]lose', silent = true })
+
+-- Navigate to next/previous tab
+vim.keymap.set('n', '<leader>tl', ':tabnext<CR>', { desc = '[T]ab Next (Right)', silent = true })
+vim.keymap.set('n', '<leader>th', ':tabprevious<CR>', { desc = '[T]ab Previous (Left)', silent = true })
+
+-- Alternative navigation with shift-h and shift-l (common vim approach)
+vim.keymap.set('n', '<S-l>', ':tabnext<CR>', { desc = 'Tab Next', silent = true })
+vim.keymap.set('n', '<S-h>', ':tabprevious<CR>', { desc = 'Tab Previous', silent = true })
+
+-- Go to specific tab by number (1-9)
+-- for i = 1, 9 do
+--   vim.keymap.set('n', '<leader>' .. i, i .. 'gt', { desc = 'Go to tab ' .. i, silent = true })
+-- end
+
+-- Go to last tab
+vim.keymap.set('n', '<leader>tL', ':tablast<CR>', { desc = '[T]ab [L]ast', silent = true })
+
+-- Go to first tab
+vim.keymap.set('n', '<leader>tF', ':tabfirst<CR>', { desc = '[T]ab [F]irst', silent = true })
+
+-- Move current tab left/right
+vim.keymap.set('n', '<leader>tmh', ':-tabmove<CR>', { desc = '[T]ab [M]ove Left', silent = true })
+vim.keymap.set('n', '<leader>tml', ':+tabmove<CR>', { desc = '[T]ab [M]ove Right', silent = true })
+
+-- List all tabs
+vim.keymap.set('n', '<leader>ts', ':tabs<CR>', { desc = '[T]ab [S]how All', silent = true })
 -- vim: ts=2 sts=2 sw=2 et
