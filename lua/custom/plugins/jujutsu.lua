912863d454c9f6aea6aabfa64eca4ecdcfd44e78
Taylor Eernisse <teernisse@visiostack.com>
2025-04-08 20:04:07 -0400

    update jujutsu plugin definition

--- lua/custom/plugins/jujutsu.lua
+++ lua/custom/plugins/jujutsu.lua
@@ -24,19 +24,27 @@ return {
   --     require('jujutsu').setup()
   --   end
   -- },
-  {
-    dir = '~/jujutsu-me.nvim',
-    config = function()
-      require('jujutsu').setup()
-    end
-  }
   -- {
-  --   'solsystemlabs/jujutsu.nvim',
+  --   dir = "~/jj-grok",
+  --   dependencies = { 'powerman/vim-plugin-AnsiEsc' },
   --   config = function()
   --     require('jujutsu').setup()
-  --   end,
-  --   dependencies = {
-  --     -- Optional dependencies, if you want to use them
-  --   }
+  --   end
   -- }
+  -- {
+  --   dir = '~/jujutsu-me.nvim',
+  --   config = function()
+  --     require('jujutsu').setup()
+  --   end
+  -- }
+  {
+    'solsystemlabs/jujutsu.nvim',
+    config = function()
+      require('jujutsu').setup()
+    end,
+    dependencies = {
+      -- Optional dependencies, if you want to use them
+      'powerman/vim-plugin-AnsiEsc'
+    }
+  }
 }
