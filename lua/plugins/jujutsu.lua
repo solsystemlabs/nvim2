6cfc835842d132a68372295ea29ad8b1c3f2d416
Taylor Eernisse <teernisse@visiostack.com>
2025-04-10 17:31:09 -0400

    use dev config for jujutsu

--- lua/plugins/jujutsu.lua
+++ lua/plugins/jujutsu.lua
@@ -1,16 +1,17 @@
 return {
+  -- {
+  --   'solsystemlabs/jujutsu.nvim',
+  --   dependencies = { 'powerman/vim-plugin-AnsiEsc' },
+  --   config = function()
+  --     require('jujutsu').setup()
+  --   end,
+  -- }
   {
     'solsystemlabs/jujutsu.nvim',
+    branch = 'develop',
     dependencies = { 'powerman/vim-plugin-AnsiEsc' },
     config = function()
       require('jujutsu').setup()
     end,
   }
-  -- {
-  --   dir = "~/jj-grok",
-  --   dependencies = { 'powerman/vim-plugin-AnsiEsc' },
-  --   config = function()
-  --     require('jujutsu').setup()
-  --   end
-  -- }
 }
