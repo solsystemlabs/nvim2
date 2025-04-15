6b8d8b46f320d7aaeee8cd4221701b77fadf3e68
Taylor Eernisse <teernisse@visiostack.com>
2025-04-15 10:04:28 -0400

    Swap keybinds for smart search and recent files search

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -72,11 +72,11 @@ return { {
   },
   keys = {
     -- Top Pickers & Explorer
-    { "<leader>.",       function() require('telescope.builtin').oldfiles() end,                  desc = "Search Recent Files" },
+    { "<leader>.",       function() Snacks.picker.smart() end,                                    desc = "Smart Find Files" },
     { "<leader>,",       function() Snacks.picker.buffers() end,                                  desc = "Buffers" },
     { "<leader>/",       function() Snacks.picker.grep() end,                                     desc = "Grep" },
     { "<leader>:",       function() Snacks.picker.command_history() end,                          desc = "Command History" },
-    { "<leader><space>", function() Snacks.picker.smart() end,                                    desc = "Smart Find Files" },
+    { "<leader><space>", function() require('telescope.builtin').oldfiles() end,                  desc = "Search Recent Files" },
     { "<leader>e",       function() Snacks.explorer() end,                                        desc = "File Explorer" },
     { "<leader>n",       function() Snacks.picker.notifications() end,                            desc = "Notification History" },
 
