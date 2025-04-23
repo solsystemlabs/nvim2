3ef04df72aa2ab648b63e6c3c656a97f12509527
Taylor Eernisse <teernisse@visiostack.com>
2025-04-23 16:12:48 -0400

    jujutsu local dev

--- lua/plugins/vcs/jujutsu.lua
+++ lua/plugins/vcs/jujutsu.lua
@@ -1,17 +1,17 @@
 return {
-  {
-    'solsystemlabs/jujutsu.nvim',
-    branch = 'develop',
-    dependencies = { 'powerman/vim-plugin-AnsiEsc' },
-    config = function()
-      require('jujutsu').setup()
-    end,
-  }
   -- {
-  --   dir = "~/jj-grok",
+  --   'solsystemlabs/jujutsu.nvim',
+  --   branch = 'develop',
   --   dependencies = { 'powerman/vim-plugin-AnsiEsc' },
   --   config = function()
   --     require('jujutsu').setup()
-  --   end
+  --   end,
   -- }
+  {
+    dir = "~/jj-grok",
+    dependencies = { 'powerman/vim-plugin-AnsiEsc' },
+    config = function()
+      require('jujutsu').setup()
+    end
+  }
 }
