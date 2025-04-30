793ac333c0a074342500ee499bf68a80fa94c73f
Taylor Eernisse <teernisse@visiostack.com>
2025-04-30 09:21:22 -0400

    remove reference to 'colorscripts' as I don't want to have to install third party things

--- lua/plugins/core/snacks.lua
+++ lua/plugins/core/snacks.lua
@@ -14,13 +14,6 @@ return { {
     dashboard = {
       sections = {
         { section = "header" },
-        {
-          pane = 2,
-          section = "terminal",
-          cmd = "colorscript -e square",
-          height = 5,
-          padding = 1,
-        },
         { section = "keys", gap = 1, padding = 1 },
         { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
         { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
