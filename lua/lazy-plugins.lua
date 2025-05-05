55a2ef080c7d4461e2bb21feb936a061aef6835c
Taylor Eernisse <teernisse@visiostack.com>
2025-05-05 16:00:29 -0400

    promote jsx-element to core

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -3,6 +3,7 @@ require('lazy').setup({
 
   -- require 'plugins.core.cmp',
   require 'plugins.core.conform',
+  require 'plugins.core.jsx-element',
   require 'plugins.core.nvim-surround',
   require 'plugins.core.snacks',
   require 'plugins.core.telescope',
@@ -13,8 +14,6 @@ require('lazy').setup({
   require 'plugins.core.which-key',
   require 'plugins.experimental.blink',
   require 'plugins.experimental.dadbod-explorer',
-  require 'plugins.experimental.fzf-lua',
-  require 'plugins.experimental.jsx-element',
   require 'plugins.experimental.nvim-scissors',
   require 'plugins.experimental.typr',
   require 'plugins.experimental.yazi',
