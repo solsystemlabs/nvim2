d0f757696e2bee4e331e3822e5241c7fd7b40f61
Taylor Eernisse <teernisse@visiostack.com>
2025-05-21 22:47:32 -0700

    add hbac

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -17,6 +17,7 @@ require('lazy').setup({
   require 'plugins.experimental.bookmarks',
   require 'plugins.experimental.dadbod-explorer',
   require 'plugins.experimental.dropbar',
+  require 'plugins.experimental.hbac',
   require 'plugins.experimental.leap',
   require 'plugins.experimental.nvim-numbertoggle',
   require 'plugins.experimental.overseer',
