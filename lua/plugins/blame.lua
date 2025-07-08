7ad65df29baeab591a142465cd2f24875ed06f57
teernisse <teernisse@visiostack.com>
2025-07-08 13:54:08 -0400

    misc keymap updates and other plugin updates

---
+++ lua/plugins/blame.lua
@@ -0,0 +1,12 @@
+return {
+  "FabijanZulj/blame.nvim",
+  config = function()
+    require("blame").setup({
+      width = 50,
+      format = "%s | %t | %a",
+      date_format = "%Y-%m-%d",
+      max_summary_width = 30,
+      virtual_style = "right_align",
+    })
+  end,
+}
--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -25,6 +25,107 @@ return {
         end,
         desc = "Recent files (global)",
       },
+      -- Override default file search commands to always use cwd
+      {
+        "<leader>ff",
+        function()
+          Snacks.picker.files({ cwd = vim.fn.getcwd() })
+        end,
+        desc = "Find Files (cwd)",
+      },
+      {
+        "<leader>fg",
+        function()
+          Snacks.picker.git_files({ cwd = vim.fn.getcwd() })
+        end,
+        desc = "Find Git Files (cwd)",
+      },
+      {
+        "<leader><space>",
+        function()
+          Snacks.picker.recent({ filter = { cwd = true } })
+        end,
+        desc = "Find Recent Files (cwd)",
+      },
+      -- Swap search functionality: sg -> cwd, sG -> root
+      {
+        "<leader>sg",
+        function()
+          Snacks.picker.grep({ cwd = vim.fn.getcwd() })
+        end,
+        desc = "Grep (cwd)",
+      },
+      {
+        "<leader>sG",
+        function()
+          Snacks.picker.grep()
+        end,
+        desc = "Grep (Root Dir)",
+      },
+      {
+        "<leader>sw",
+        function()
+          Snacks.picker.grep_word({ cwd = vim.fn.getcwd() })
+        end,
+        desc = "Word (cwd)",
+      },
+      {
+        "<leader>sW",
+        function()
+          Snacks.picker.grep_word()
+        end,
+        desc = "Word (Root Dir)",
+      },
     },
+  },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        default = { "copilot" },
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-cmp-copilot",
+            kind = "Copilot",
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+    },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    optional = true,
+    opts = function()
+      local Offset = require("bufferline.offset")
+      if not Offset.edgy then
+        local get = Offset.get
+        Offset.get = function()
+          if package.loaded.edgy then
+            local old_offset = get()
+            local layout = require("edgy.config").layout
+            local ret = { left = "", left_size = 0, right = "", right_size = 0 }
+            for _, pos in ipairs({ "left", "right" }) do
+              local sb = layout[pos]
+              local title = " Sidebar" .. string.rep(" ", sb.bounds.width - 8)
+              if sb and #sb.wins > 0 then
+                ret[pos] = old_offset[pos .. "_size"] > 0 and old_offset[pos]
+                  or pos == "left" and ("%#Bold#" .. title .. "%*" .. "%#BufferLineOffsetSeparator#│%*")
+                  or pos == "right" and ("%#BufferLineOffsetSeparator#│%*" .. "%#Bold#" .. title .. "%*")
+                ret[pos .. "_size"] = old_offset[pos .. "_size"] > 0 and old_offset[pos .. "_size"] or sb.bounds.width
+              end
+            end
+            ret.total_size = ret.left_size + ret.right_size
+            if ret.total_size > 0 then
+              return ret
+            end
+          end
+          return get()
+        end
+        Offset.edgy = true
+      end
+    end,
   },
 }
