6d748de30057933ef29df5bc05a5185fb92af5ca
teernisse <teernisse@visiostack.com>
2025-07-29 16:46:53 -0400

    change color theme to cyberdream

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,20 @@
 return {
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     require("gruvbox").setup({
+  --       invert_tabline = true,
+  --       -- contrast = "hard",
+  --       transparent_mode = false,
+  --     })
+  --     require("gruvbox").load()
+  --     -- vim.cmd.colorscheme("gruvbox")
+  --   end,
+  -- },
   {
-    "ellisonleao/gruvbox.nvim",
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
     priority = 1000,
-    config = function()
-      require("gruvbox").setup({
-        invert_tabline = true,
-        -- contrast = "hard",
-        transparent_mode = false,
-      })
-      require("gruvbox").load()
-      vim.cmd.colorscheme("gruvbox")
-    end,
   },
 }
--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -2,7 +2,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "cyberdream",
       news = {
         neovim = true,
       },
@@ -124,7 +124,7 @@ return {
         desc = "Notification History (Picker)",
       },
       {
-        "<leader>N", 
+        "<leader>N",
         function()
           Snacks.notifier.show_history()
         end,
@@ -141,6 +141,41 @@ return {
   {
     "saghen/blink.cmp",
     opts = {
+      completion = {
+        menu = {
+          draw = {
+            -- We don't need label_description now because label and label_description are already
+            -- combined together in label by colorful-menu.nvim.
+            columns = { { "kind_icon" }, { "label", gap = 1 } },
+            components = {
+              label = {
+                width = { fill = true, max = 60 },
+                text = function(ctx)
+                  local highlights_info = require("colorful-menu").blink_highlights(ctx)
+                  if highlights_info ~= nil then
+                    -- Or you want to add more item to label
+                    return highlights_info.label
+                  else
+                    return ctx.label
+                  end
+                end,
+                highlight = function(ctx)
+                  local highlights = {}
+                  local highlights_info = require("colorful-menu").blink_highlights(ctx)
+                  if highlights_info ~= nil then
+                    highlights = highlights_info.highlights
+                  end
+                  for _, idx in ipairs(ctx.label_matched_indices) do
+                    table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
+                  end
+                  -- Do something else
+                  return highlights
+                end,
+              },
+            },
+          },
+        },
+      },
       sources = {
         default = { "copilot" },
         providers = {
